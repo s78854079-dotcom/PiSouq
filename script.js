8f7d74ba902872c440fedd2b5f3187ea2be3376b
s78854079-dotcom <s78854079@gmail.com>
2026-07-07 19:49:55 +0300

    Implement image upload for product saving
    Added image handling to product saving functionality.

--- script.js
+++ script.js
@@ -4,11 +4,39 @@ function addProduct() {
     let price = document.getElementById("price").value;
     let description = document.getElementById("description").value;
 
+    let imageInput = document.getElementById("image");
+
+    let image = "";
+
+    if (imageInput.files.length > 0) {
+        let reader = new FileReader();
+
+        reader.onload = function () {
+
+            image = reader.result;
+
+            saveProduct(name, price, description, image);
+
+        };
+
+        reader.readAsDataURL(imageInput.files[0]);
+
+    } else {
+
+        saveProduct(name, price, description, image);
+
+    }
+
+}
+
+
+function saveProduct(name, price, description, image) {
 
     let product = {
         name: name,
         price: price,
-        description: description
+        description: description,
+        image: image
     };
 
 

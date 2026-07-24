dc676c6e6506574bfb8c4b177f3567b51f9c20b5
s78854079-dotcom <s78854079@gmail.com>
2026-07-24 20:02:41 +0300

    Remove Firebase setup and authentication logic
    Removed Firebase initialization and related code.

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDE7IJclVGfx3YqRtcDrbh2oY9eevyWFIQ",
-  authDomain: "pisouq-4d7da.firebaseapp.com",
-  projectId: "pisouq-4d7da",
-  storageBucket: "pisouq-4d7da.firebasestorage.app",
-  messagingSenderId: "884828799012",
-  appId: "1:884828799012:web:9c6ceb500cf9c47ea2d8b1"
-};
-
-const const = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-if (typeof Pi !== "undefined") {
-  Pi.init({
-    version: "2.0",
-    sandbox: false
-  });
-}
-window.loginWithPi = async function () {
-
-  try {
-
-    const auth = await Pi.authenticate(["username"]);
-
-    localStorage.setItem("username", auth.user.username);
-
-    alert("تم تسجيل الدخول: " + auth.user.username);
-
     location.reload();
 
   } catch (e) {
@@ -80,5 +44,4 @@ window.addProduct = async function () {
     alert("حدث خطأ أثناء النشر");
 
   }
-
-};
+  

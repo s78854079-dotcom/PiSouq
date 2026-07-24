d6f86ada9b9ecaa6232ca4699d24f5e9651c81e2
s78854079-dotcom <s78854079@gmail.com>
2026-07-24 19:48:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDE7IJclVGfx3YqRtcDrbh2oY9eevyWFIQ",
@@ -9,35 +15,12 @@ const firebaseConfig = {
   appId: "1:884828799012:web:9c6ceb500cf9c47ea2d8b1"
 };
 
-initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 if (typeof Pi !== "undefined") {
-Pi.init({
-  version: "2.0",
-  sandbox: false
-});
+  Pi.init({
+    version: "2.0",
+    sandbox: false
+  });
 }
-
-
-window.loginWithPi = function () {
-
-  Pi.authenticate(
-    ["username"],
-    function(auth) {
-
-      localStorage.setItem("username", auth.user.username);
-
-      alert("تم تسجيل الدخول: " + auth.user.username);
-
-      location.reload();
-
-    },
-    function(error) {
-
-      console.log(error);
-
-      alert("فشل تسجيل الدخول");
-
-   );
-};

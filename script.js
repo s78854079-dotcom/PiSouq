e670be1c229ef726ef4e0c533b8da6a1fdf9b14f
s78854079-dotcom <s78854079@gmail.com>
2026-08-13 20:47:22 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+    location.reload();
+
+  } catch (e) {
+
+    alert("فشل تسجيل الدخول");
+
+    console.log(e);
+
+  }
+
+};
+
+window.addProduct = async function () {
+
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const description = document.getElementById("description").value;
+
+  const username = localStorage.getItem("username") || "مستخدم";
+
+  if (!name || !price || !description) {
+    alert("يرجى تعبئة جميع الحقول");
+    return;
+  }
+
+  try {
+
+    await addDoc(collection(db, "products"), {
+      name: name,
+      price: Number(price),
+      description: description,
+      seller: username,
+      createdAt: Date.now()
+    });
+
+    alert("تم نشر الإعلان بنجاح ✅");
+
+    location.href = "buy.html";
+
+  } catch (e) {
+
+    console.log(e);
+
+    alert("حدث خطأ أثناء النشر");
+
+  }
+
+    
